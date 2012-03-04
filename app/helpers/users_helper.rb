b7175d8666085b79e6f0ba9df23e629d795fb989
karidyang <karidyang@gmail.com>
2012-03-04 17:52:12 +0800

    修正代码规范

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@
 module UsersHelper
   def user_name_tag(user)
     result = '来访者'
-    if (user)
+    if user
       result = "<a href=\"#{user_path(user.id)}\" title=\"#{user.name}\">#{user.name}</a>"
     end
     raw(result)
