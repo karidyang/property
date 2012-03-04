cdafe8655f42fe72ec9718e3e394ada92a1a85e8
karidyang <karidyang@gmail.com>
2012-03-04 14:52:32 +0800

    初始化用户小区

--- db/migrate/20111224160845_add_plots_users.rb
+++ db/migrate/20111224160845_add_plots_users.rb
@@ -1,9 +1,14 @@
 class AddPlotsUsers < ActiveRecord::Migration
   def change
-    create_table :plots_users, :id=>false do |t|
+    create_table :plots_users, :id => false do |t|
       t.integer :plot_id, :null => false
       t.integer :user_id, :null => false
     end
   end
 
+  u = User.first
+  unless u.nil?
+    u.plots << Plot.find(1)
+    u.save
+  end
 end
